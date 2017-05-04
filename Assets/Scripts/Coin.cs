20a0c6597600e521831f31428d6d2b7d19d4bce1
Sean MacLarion <maclaris1@hawkmail.newpaltz.edu>
2017-05-04 18:40:56 -0400

    Final

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Picked up coins");
+        var player = collision.gameObject.GetComponent<Player>();
+        if (player != null)
+        {
+            gameObject.GetComponent<Collider2D>().enabled = false;
+            gameObject.GetComponent<SpriteRenderer>().enabled = false;
+            FindObjectOfType<GM>().SetPoints(FindObjectOfType<GM>().GetPoints() + 1);
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/Sniper.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sniper : Weapon
+{
+
+    public bool isActive = false;
+
+    void Update()
+    {
+    }
+
+
+    public override void Attack()
+    {
+        Shoot();
+    }
+
+    public override void GetPickedUp(Player player)
+    {
+        if (isActive)
+        {
+            return;
+        }
+        isActive = true;
+        base.GetPickedUp(player);
+        transform.localScale = new Vector3(.075f, .075f);
+        transform.localPosition = new Vector3(.3f, -.1f);
+    }
+
+    
+    public void Shoot()
+    {
+        //  Get a reference to all enemies
+        var enemies = FindObjectsOfType<Enemy>();
+
+        //  Loop through each enemy in the list
+        foreach (var e in enemies)
+        {
+
+            //  Check if that enemy is within the sniper scope
+            if ((e.transform.position.y < transform.position.y + 1) && (e.transform.position.y > transform.position.y - 1) && (e.transform.position.x > transform.position.x))
+            {
+
+                //  Set that enemy to NOT-Active
+                e.gameObject.SetActive(false);
+            }
+        }
+
+        //  Set myself (aka the gun) to NOT-Active. That way the sniper disappears, and can not be picked up again.
+        gameObject.SetActive(false);
+        Player.currentWeapon = null;
+    }
+}
